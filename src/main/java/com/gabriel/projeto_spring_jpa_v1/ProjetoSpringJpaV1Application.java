c92dcd2aec56ff662955031a391f7c5b243730c1
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-03-09 19:18:01 -0300

    first commit

---
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/ProjetoSpringJpaV1Application.java
@@ -0,0 +1,13 @@
+package com.gabriel.projeto_spring_jpa_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJpaV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringJpaV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/projeto_spring_jpa_v1/ProjetoSpringJpaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.projeto_spring_jpa_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringJpaV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
