1570d6e19e333f3663c0222ac1f82b01f18bd894
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-03-13 21:15:22 -0300

    - criei os repository para os cliente e para o operador. -adicionei dois metodos do jpa, findBySenha e indByEmail, para realizar o login.

---
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.gabriel.projeto_spring_jpa_v1.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriel.projeto_spring_jpa_v1.model.Cliente;
+
+
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByEmail(String email);
+    Optional<Cliente> findBySenha(String senha);
+    
+}
---
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/repository/OperadorReposytory.java
@@ -0,0 +1,10 @@
+package com.gabriel.projeto_spring_jpa_v1.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.gabriel.projeto_spring_jpa_v1.model.Operador;
+
+public interface OperadorReposytory extends JpaRepository<Operador, Long>{
+    Optional<Operador> findByEmail(String email);
+}
