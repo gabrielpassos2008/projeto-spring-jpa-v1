123f13b366565a86884a5aa849e8abcd2573739c
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-03-18 21:43:12 -0300

    - metodo que pega todos os nome do serch, se pesquisar jo, ele retorna joão

--- src/main/java/com/gabriel/projeto_spring_jpa_v1/repository/ClienteRepository.java
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
 package com.gabriel.projeto_spring_jpa_v1.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,8 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     Optional<Cliente> findByEmail(String email);
 
     Optional<Cliente> findBySenha(String senha);
-    
+
+    List<Cliente> findByNomeContainingIgnoreCase(String nome);
+
     Optional<Cliente> findByEmailAndSenha(String email, String senha);
 }
--- src/main/java/com/gabriel/projeto_spring_jpa_v1/repository/OperadorRepository.java
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/repository/OperadorRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gabriel.projeto_spring_jpa_v1.model.Operador;
 
+
 public interface OperadorRepository extends JpaRepository<Operador, Long>{
     Optional<Operador> findByEmail(String email);
-
+    
     Optional<Operador> findByEmailAndSenha(String email, String senha);
 }
