8ebc6bf4d897ac25ecb4b0f3b2013c97a0e45c28
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-03-19 21:32:57 -0300

    - class service divida, nela que sera feito o abater divida

---
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/service/DividaService.java
@@ -0,0 +1,37 @@
+package com.gabriel.projeto_spring_jpa_v1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.projeto_spring_jpa_v1.model.Cliente;
+import com.gabriel.projeto_spring_jpa_v1.model.Divida;
+import com.gabriel.projeto_spring_jpa_v1.repository.ClienteRepository;
+import com.gabriel.projeto_spring_jpa_v1.repository.DividaRepository;
+
+import lombok.var;
+@Service
+public class DividaService {
+    @Autowired
+    private DividaRepository dividaRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public boolean abaterDivida(Long clienteId,int valor){
+        var consultaCliente = clienteRepository.findById(clienteId);
+        if (consultaCliente.isEmpty()) {
+            return false;
+        }
+        if (valor <= 0) {
+            return false;
+        }
+        Cliente cliente = consultaCliente.get();
+        Divida divida = new Divida();
+        divida.setValor(-valor);
+        divida.setCliente(cliente);
+        divida.setStatus("ABATIMENTO");
+
+        dividaRepository.save(divida);
+        return true;
+    }
+}
