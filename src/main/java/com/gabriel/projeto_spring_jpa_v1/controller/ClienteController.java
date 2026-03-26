20dc9272849f3bde78a366ead26028a8a1790c8a
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-03-26 19:51:54 -0300

    - rotas get do cliente finalizadas.

--- src/main/java/com/gabriel/projeto_spring_jpa_v1/controller/ClienteController.java
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/controller/ClienteController.java
@@ -1,37 +1,83 @@
 package com.gabriel.projeto_spring_jpa_v1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gabriel.projeto_spring_jpa_v1.model.Cliente;
 
 import com.gabriel.projeto_spring_jpa_v1.service.ClienteService;
+import com.gabriel.projeto_spring_jpa_v1.service.DividaService;
+import com.gabriel.projeto_spring_jpa_v1.service.OPeradorService;
 
 import jakarta.servlet.http.HttpSession;
+
+
 @Controller
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private DividaService dividaService;
     
-    private ClienteService service;
+    @Autowired
+    private OPeradorService operadorService;
 
-    public ClienteController(ClienteService clienteService){
-        this.service = clienteService;
-    } 
-
-    @GetMapping("/")
+    @GetMapping("/login")
     public String getLogin() {
         return "cliente/login-cliente";
     }
     
-    @PostMapping("/")
+    @PostMapping("/login")
     public String postLogin(Cliente cliente, HttpSession session) {
-        if(service.validarLogin(cliente.getEmail(),cliente.getSenha())) {
+        if(clienteService.validarLogin(cliente.getEmail(),cliente.getSenha())) {
             session.setAttribute("cliente", cliente);
-            return "operador/pesquisar-usuario";
+            return "redirect:/";
         } else {
             return "cliente/login-cliente";
         }
         
+    }
+
+    @GetMapping("/")
+    public String getDeshboard(HttpSession session) {
+        if (session.getAttribute("cliente") == null) {
+            return "redirect:/login";
+        }
+        return "cliente/deshboard-cliente";
+    }
+    
+    @GetMapping("/historico")
+    public String getHistorico(HttpSession session) {
+        if (session.getAttribute("cliente") == null) {
+            return "redirect:/login";
+        }
+        return "cliente/historico-cliente";
+    }
+
+    @GetMapping("/perfil")
+    public String getPerfil(HttpSession session) {
+        if (session.getAttribute("cliente") == null) {
+            return "redirect:/login";
+        }
+        return "cliente/perfil-cliente";
+    }
+
+    @GetMapping("/perfil/editar")
+    public String getEditarPerfil(HttpSession session) {
+        if (session.getAttribute("cliente") == null) {
+            return "redirect:/login";
+        }
+        return "cliente/editar-Perfil-cliente";
+    }
+    
+    @GetMapping("/sair")
+    public String getSair(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
     }
     
 }
