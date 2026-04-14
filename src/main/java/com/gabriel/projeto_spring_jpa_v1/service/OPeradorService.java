adad5b7bc195da0441ce98ac1ccbf6337e79522e
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-04-14 19:41:19 -0300

    - criei o metodo para validar a quantidade de carecteres nso campos.

--- src/main/java/com/gabriel/projeto_spring_jpa_v1/service/OPeradorService.java
+++ src/main/java/com/gabriel/projeto_spring_jpa_v1/service/OPeradorService.java
@@ -1,5 +1,7 @@
 package com.gabriel.projeto_spring_jpa_v1.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import com.gabriel.projeto_spring_jpa_v1.repository.OperadorRepository;
 
 @Service
 public class OPeradorService {
-
+    List<String> erros = new ArrayList<>();
     @Autowired
     private OperadorRepository repository;
 
@@ -24,5 +26,54 @@ public class OPeradorService {
 
     public void salvar(Operador operador) {
         repository.save(operador);
+    }
+
+    public List<String> editarOperador(Operador operador) {
+        erros.clear();
+        if (validaDadosClienteEditar(operador)) {
+            repository.save(operador);
+            return null;
+        }
+        return erros;
+    }
+
+    public boolean validaDadosClienteEditar(Operador operador) {
+        if (validarCampoNome(operador.getNome()) && validarCampoEmail(operador.getEmail())
+                && validarCampoSenha(operador.getSenha()) && validarCampotelefone(operador.getTelefone())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validarCampoNome(String nome) {
+        if (nome.length() <= 70 && nome.length() >= 2) {
+            return true;
+        }
+        erros.add("Verificar nome do cliente");
+        return false;
+    }
+
+    public boolean validarCampotelefone(String telefone) {
+        if (telefone.length() <= 15 && telefone.length() >= 10) {
+            return true;
+        }
+        erros.add("Verificar telefone do cliente");
+        return false;
+    }
+
+    public boolean validarCampoEmail(String email) {
+        if (email.length() <= 120 && email.length() >= 5) {
+            return true;
+        }
+        erros.add("Verificar Email do cliente");
+        return false;
+    }
+
+    public boolean validarCampoSenha(String senha) {
+        if (senha.length() <= 64 && senha.length() >= 8) {
+            return true;
+        }
+        erros.add("Verificar senha do cliente");
+        return false;
     }
 }
